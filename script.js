6275cbc9b420d138b363e43238856c8b10396c8c
Gyovai-Rafa Jeremy <jeyrgamer@gmail.com>
2020-04-25 16:21:56 +0200

    jeremy language localization commit,

--- script.js
+++
@@ -1,9 +0,0 @@
-function greyPattern(){
-    document.body.style.backgroundColor = "#e6e6e6";
-    document.getElementById("main_container").style.borderColor="#333333";
-}
-
-function defaultPattern(){
-    document.body.style.backgroundColor = "rgb(246,181,23)";
-    document.getElementById("main_container").style.borderColor="rgb(21,103,147)";
-}

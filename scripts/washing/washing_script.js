6e2bf43aa4bc2468a2c361c87bf0969dcc47eb78
MKInABox <kissmartina97@gmail.com>
2020-04-28 20:35:44 +0200

    Washing bug resolved, two more machine tables created

--- scripts/washing/washing_script.js
+++ scripts/washing/washing_script.js
@@ -2,17 +2,51 @@
 var machine_1 = [
     { idopont: "8:00-9:00", hetfo: "", kedd:"" , szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "9:00-10:00",hetfo: "", kedd: "" , szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
-    { idopont: "10:00-11:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "10:00-11:00",hetfo: "QWE123", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "11:00-12:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
-    { idopont: "12:00-13:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "12:00-13:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "RTZ456", szombat: "", vasarnap: "" },
     { idopont: "13:00-14:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "14:00-15:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "15:00-16:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "16:00-17:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "17:00-18:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
     { idopont: "18:00-19:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
-    { idopont: "19:00-20:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "19:00-20:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "QWE123", vasarnap: "" },
     { idopont: "21:00-22:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "22:00-23:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" }
+];
+
+var machine_2 = [
+    { idopont: "8:00-9:00", hetfo: "", kedd:"" , szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "9:00-10:00",hetfo: "", kedd: "" , szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "10:00-11:00",hetfo: "", kedd: "", szerda: "", csutortok: "UIO789", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "11:00-12:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "12:00-13:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "13:00-14:00",hetfo: "", kedd: "", szerda: "", csutortok: "ASD123", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "14:00-15:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "15:00-16:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "16:00-17:00",hetfo: "", kedd: "RTZ456", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "17:00-18:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "18:00-19:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "19:00-20:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "UIO789", vasarnap: "" },
+    { idopont: "21:00-22:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "22:00-23:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" }
+];
+
+var machine_3 = [
+    { idopont: "8:00-9:00", hetfo: "ASD123", kedd:"" , szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "9:00-10:00",hetfo: "", kedd: "" , szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "10:00-11:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "11:00-12:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "12:00-13:00",hetfo: "", kedd: "", szerda: "", csutortok: "FGH456", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "13:00-14:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "14:00-15:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "15:00-16:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "16:00-17:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "17:00-18:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "18:00-19:00",hetfo: "", kedd: "FGH456", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "19:00-20:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" },
+    { idopont: "21:00-22:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "JKL789" },
     { idopont: "22:00-23:00",hetfo: "", kedd: "", szerda: "", csutortok: "", pentek: "", szombat: "", vasarnap: "" }
 ];
 
@@ -26,14 +60,10 @@ function generateTableHead(tableConst, dataConst) {
     row.appendChild(th);
   }
 }
-var table1 = document.getElementById("table1");
-var data = Object.keys(machine_1[0]);
 
-generateTableHead(table1, data);
-
-function generateTable(table1, data){
+function generateTable(table, data){
     for(var element of data){
-        var row = table1.insertRow();
+        var row = table.insertRow();
         for (key in element){
             var cell = row.insertCell();
             var text = document.createTextNode(element[key]);
@@ -42,7 +72,20 @@ function generateTable(table1, data){
     }
 }
 
+var table1 = document.getElementById("table1");
+var data1 = Object.keys(machine_1[0]);
+generateTableHead(table1, data1);
 generateTable(table1, machine_1);
+
+var table2 = document.getElementById("table2");
+var data2 = Object.keys(machine_2[0]);
+generateTableHead(table2, data2);
+generateTable(table2, machine_2);
+//
+var table3 = document.getElementById("table3");
+var data3 = Object.keys(machine_3[0]);
+generateTableHead(table3, data3);
+generateTable(table3, machine_3);
 
 
 var tableDataStyle = document.getElementsByTagName('td');
@@ -50,8 +93,6 @@ var tableDataStyle = document.getElementsByTagName('td');
 
 
 ////here I set the style of the table data (except for the tablehead and the first column)
-
-
 
 for (var i = 0; i < tableDataStyle.length; i++){
     if(i!=0 && i%8 != 0){
@@ -67,37 +108,82 @@ for (var i = 0; i < tableDataStyle.length; i++){
     }
 }
 
+
+function setReservedCells(table){
+  for(var i = 1; i < 15; i++){
+      for(var j=1; j<8; j++){
+        if(table.rows[i].cells[j].innerHTML != ""){
+          table.rows[i].cells[j].style.backgroundColor = "rgba(255,0,0,0.6)";
+        }
+      }
+  }
+}
+
+setReservedCells(table1);
+setReservedCells(table2);
+setReservedCells(table3);
+
 ////here I add event listener to the table elements
 var reserveCounter = 0;
 
-for(var i=1; i<15; i++){
-    for(var j=1; j<8; j++){
-        table1.rows[i].cells[j].addEventListener("click", function(){
-            this.style.backgroundColor = "rgba(255,0,0,0.6)";
-            this.innerHTML = "F51ZA1";
-            this.style.transition = "400ms";
-            reserveCounter++;
-            if (reserveCounter >= 3){
-                this.removeEventListener("click", function(){})
-                console.log("you can only res 2");
-                this.style.backgroundColor = "white";
-                this.innerHTML = "";
-                reserveCounter = 0;
+function addEventListenersToTable(table){
+  for(var i=1; i<15; i++){
+      for(var j=1; j<8; j++){
+          table.rows[i].cells[j].addEventListener("click", function(){
+            if(this.innerHTML == ""){
+              this.style.backgroundColor = "rgba(255,0,0,0.6)";
+              this.innerHTML = "F51ZA1";
+              this.style.transition = "400ms";
+              reserveCounter++;
+              if (reserveCounter >= 3){
+                  this.removeEventListener("click", function(){})
+                  console.log("you can only res 2");
+                  this.style.backgroundColor = "white";
+                  this.innerHTML = "";
+              }
             }
-        })
-    }
+            else {
+              this.removeEventListener("click", function(){})
+            }
+          })
+      }
+  }
 }
+
+addEventListenersToTable(table1);
+addEventListenersToTable(table2);
+addEventListenersToTable(table3);
 
 ////here I remove the reservations via button
 
 document.getElementById("remove_reservation").addEventListener("click", function(){
     for(var i=1; i<15; i++){
         for(var j=1; j<8; j++){
+          if(table1.rows[i].cells[j].innerHTML == "F51ZA1"){
             table1.rows[i].cells[j].innerHTML = "";
             table1.rows[i].cells[j].style.backgroundColor = "white";
             reserveCounter = 0;
         }
+      }
+    }
+
+    for(var i=1; i<15; i++){
+        for(var j=1; j<8; j++){
+          if(table2.rows[i].cells[j].innerHTML == "F51ZA1"){
+            table2.rows[i].cells[j].innerHTML = "";
+            table2.rows[i].cells[j].style.backgroundColor = "white";
+            reserveCounter = 0;
+        }
+      }
+    }
+
+    for(var i=1; i<15; i++){
+        for(var j=1; j<8; j++){
+          if(table3.rows[i].cells[j].innerHTML == "F51ZA1"){
+            table3.rows[i].cells[j].innerHTML = "";
+            table3.rows[i].cells[j].style.backgroundColor = "white";
+            reserveCounter = 0;
+        }
+      }
     }
 })
-
-

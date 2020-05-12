070b944bcb1431d3a4da82a41e7088b368765382
Bence Nagy <nabsabt@gmail.com>
2020-05-12 19:24:16 +0200

    modal image added to occasion page

---
+++ scripts/occasion/occasion_script.js
@@ -0,0 +1,23 @@
+    window.addEventListener('DOMContentLoaded', (event) => {
+        $(document).ready(function(){
+
+
+            function showWindow(){
+                $('#pop-up').fadeToggle(2000,'linear');
+
+                $('html body').css('overflow', 'hidden');
+                setTimeout(hideWindow, 4000);
+                
+            }
+
+
+            
+            function hideWindow(){
+                $('#pop-up').fadeToggle(2000, 'linear');
+                $('html body').css('overflow', 'scroll');
+            }
+            
+            setTimeout(showWindow, 1000);
+        })
+
+    });
